c843d5ac79b487e3c12cb33cca1148fbe9a0f491
Felicia <feliciachristiany96@gmail.com>
2024-03-09 13:11:03 +0700

    feat: update main logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import homeLogo from '../public/logo.png'; 
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -17,7 +19,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <header className="relative flex w-full items-center p-4 justify-start gap-2 bg-rose-700 text-white lg:flex-wrap lg:px-8">
-          <Link href="/">
+          <Link href="/" className='flex gap-2 items-center'>
+            <Image
+              src={homeLogo}
+              alt='home'
+              width={30}
+              height={30}
+            />
             <h2 className="font-bold">POKEDEX</h2>
           </Link>
         </header>
